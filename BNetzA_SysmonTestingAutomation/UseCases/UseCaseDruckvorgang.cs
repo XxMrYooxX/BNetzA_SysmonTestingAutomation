691de70f8c1af88fb6fe76b559664f5c4a520ca3
Marcel Hesselbach <ki.marcel.hesselbach@htwsaar.de>
2022-06-14 00:59:06 +0200

    Added Word Startup

--- BNetzA_SysmonTestingAutomation/UseCases/UseCaseDruckvorgang.cs
+++ BNetzA_SysmonTestingAutomation/UseCases/UseCaseDruckvorgang.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlaUI.UIA3;
+using Microsoft.Win32;
 
 namespace BNetzA_SysmonTestingAutomation.UseCases
 {
@@ -13,6 +15,19 @@ namespace BNetzA_SysmonTestingAutomation.UseCases
         public void Run()
         {
             Console.WriteLine("DEBUG Druckvorgang");
+            RegistryKey registryKeyCLSID = Registry.ClassesRoot.OpenSubKey("Word.Application\\CLSID");
+            string keyValueCLSID = (string)registryKeyCLSID.GetValue("");
+            RegistryKey registryKey = Registry.ClassesRoot.OpenSubKey("WOW6432Node\\CLSID\\" + keyValueCLSID + "\\Localserver32");
+            string pathToWord = (string) registryKey.GetValue("");
+            pathToWord = pathToWord.Replace("\"", "");
+            pathToWord = pathToWord.Replace(" /Automation", "");
+            var app = FlaUI.Core.Application.Launch(pathToWord);
+            using(var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                Console.Write(window.Title);
+            }
+
         }
     }
 }
